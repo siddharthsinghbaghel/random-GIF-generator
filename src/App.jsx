5ad2fd51cb950ec5f0c1a33bac9dd4d4c5d9db74
siddharthsinghbaghel <siddharthsingh910989@gmail.com>
2024-12-14 19:23:48 +0530

    .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import Random from "./components/Random"
+import Tag from "./components/Tag"
+
+
+
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return(
+    <div className="w-full h-screen flex flex-col background relative  overflow-x-hidden items-center">
+
+      <h1 className=" bg-sky-700 rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-4xl font-bold mx-auto">Random Gif</h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random/>
+        <Tag/>
+      </div>
+      
+    </div>
+  )
 }
